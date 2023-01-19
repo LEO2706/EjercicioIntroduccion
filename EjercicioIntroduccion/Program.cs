3bf83d4a7e6323c862b05059e3643172b8c419a5
Luis <luis.olaya@numrot.com>
2023-01-18 21:59:57 -0500

    Add project files.

---
+++ EjercicioIntroduccion/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("***************************");
+Console.WriteLine("Ejecicio #1");
+Console.WriteLine("");
+Console.Write("Mi Nombre es:");
+string nombre = Console.ReadLine();
+
+Console.WriteLine($"El nombre recibido es, {nombre}");
+
+Console.WriteLine("");
+Console.WriteLine("***************************");
+Console.WriteLine("Ejecicio #2");
+DateTime hora = DateTime.Now;
+Console.WriteLine($"La Hora es: {hora.ToString("HH:mm:ss.ff")}");

ceec9bbba3c09f739a3fc78f88b205c41b078fca
Luis Olaya <luis.olaya@numrot.com>
2023-01-18 22:59:58 -0500

    Se agrega nueva linea

--- EjercicioIntroduccion/Program.cs
+++ EjercicioIntroduccion/Program.cs
@@ -8,6 +8,7 @@ Console.WriteLine($"El nombre recibido es, {nombre}");
 
 Console.WriteLine("");
 Console.WriteLine("***************************");
+Console.WriteLine("***************************");
 Console.WriteLine("Ejecicio #2");
 DateTime hora = DateTime.Now;
 Console.WriteLine($"La Hora es: {hora.ToString("HH:mm:ss.ff")}");
